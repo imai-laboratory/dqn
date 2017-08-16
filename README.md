aae59a92905270ae71ec9156f7ce8e7e3bd1b820
Takuma Seno <takuma.seno@gmail.com>
2017-08-17 02:13:23 +0900

    TensorFlowhe (#2)
    Migrate DL framework to TensorFlow

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ DQN implementation for checking baselines.
 - Python3
 
 ## dependencies
-- chainer==2.0.0
+- tensorflow
 - gym[atari]
-- chainerrl=0.2.0
 - opencv-python
+- git+https://github.com/imai-laboratory/lightsaber
 
 ## usage
 ### training
@@ -24,6 +24,8 @@ $ python play.py --gpu {0 or -1} --render --load {path of models}
 ### cautions
 - Use `***Deterministic-v4` instead of `***-v0` for environemts because in default, environments sample {2, 3, 4} frames uniformaly. This affects negatively performance because of non-deterministic frame skipping.
 
-### TODO
-- Deep Learning framework will be changed from Chainer to Sony's NNabla.
-- Chainerrl will be removed because of its unflexiblity,
+### implementation
+This baseline is inspired by following projects.
+
+- [OpenAI Baselines](https://github.com/openai/baselines)
+- [ChainerRL](https://github.com/chainer/chainerrl)
--- train.py
+++ train.py
@@ -4,30 +4,21 @@ import gym
 import copy
 import os
 import numpy as np
+import tensorflow as tf
 
-from dqn import DQN
+from lightsaber.tensorflow.util import initialize
+from lightsaber.rl.explorer import LinearDecayExplorer
+from lightsaber.rl.replay_buffer import ReplayBuffer
 from actions import get_action_space
+from network import make_cnn
+from agent import Agent
 
-from chainer import functions as F
-from chainer import links as L
-from chainer import optimizers
-
-from chainerrl.action_value import DiscreteActionValue
-from chainerrl import agents
-from chainerrl import explorers
-from chainerrl import experiments
-from chainerrl import links
-from chainerrl import misc
-from chainerrl import replay_buffer
-
-
-def phi(states):
-    return np.asarray(states, dtype=np.float32) / 255.0
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, default='PongDeterministic-v4')
     parser.add_argument('--outdir', type=str, default=None)
+    parser.add_argument('--logdir', type=str, default=None)
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--load', type=str, default=None)
     parser.add_argument('--final-exploration-frames',
@@ -42,36 +33,38 @@ def main():
 
     if args.outdir is None:
         args.outdir = os.path.join(os.path.dirname(__file__), 'results')
+        if not os.path.exists(args.outdir):
+            os.makedirs(args.outdir)
+    if args.logdir is None:
+        args.logdir = os.path.join(os.path.dirname(__file__), 'logs')
 
     env = gym.make(args.env)
-    misc.env_modifiers.make_reward_clipped(env, -1, 1)
 
     actions = get_action_space(args.env)
     n_actions = len(actions)
 
-    q_func = links.Sequence(
-            links.NatureDQNHead(n_input_channels=args.update_interval),
-            L.Linear(512, n_actions),
-            DiscreteActionValue)
+    model = make_cnn(
+        convs=[(32, 8, 4), (64, 4, 2), (64, 3, 1)],
+        hiddens=[512]
+    )
+    replay_buffer = ReplayBuffer(10 ** 5)
+    explorer = LinearDecayExplorer(final_exploration_step=args.final_exploration_frames)
 
-    opt = optimizers.RMSpropGraves(
-        lr=2.5e-4, alpha=0.95, momentum=0.0, eps=1e-2)
-    opt.setup(q_func)
+    sess = tf.Session()
+    sess.__enter__()
 
-    rbuf = replay_buffer.ReplayBuffer(10 ** 5)
+    agent = Agent(model, n_actions, replay_buffer, explorer, learning_starts=10000)
 
-    explorer = explorers.LinearDecayEpsilonGreedy(
-        1.0, 0.1,
-        args.final_exploration_frames,
-        lambda: np.random.randint(n_actions))
+    initialize()
 
-    agent = DQN(q_func, opt, rbuf, gpu=args.gpu, gamma=0.99,
-                  explorer=explorer, replay_start_size=args.replay_start_size,
-                  target_update_interval=args.target_update_interval,
-                  update_interval=args.update_interval, phi=phi)
+    saver = tf.train.Saver()
+    if args.load is not None:
+        saver.restore(sess, args.load)
 
-    if args.load:
-        agent.load(args.load)
+    reward_summary = tf.placeholder(tf.int32, (), name='reward_summary')
+    tf.summary.scalar('reward_summary', reward_summary)
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter(args.logdir, sess.graph)
 
     global_step = 0
     episode = 0
@@ -80,6 +73,7 @@ def main():
         states = np.zeros((args.update_interval, 84, 84), dtype=np.uint8)
         reward = 0
         done = False
+        clipped_reward = 0
         sum_of_rewards = 0
         step = 0
         state = env.reset()
@@ -94,20 +88,28 @@ def main():
             states[0] = state
 
             if done:
-                agent.stop_episode_and_train(states, reward, done=done)
+                summary, _ = sess.run([merged, reward_summary], feed_dict={reward_summary: sum_of_rewards})
+                train_writer.add_summary(summary, global_step)
+                agent.stop_episode_and_train(np.transpose(states, [1, 2, 0]), clipped_reward, done=done)
                 break
 
-            action = actions[agent.act_and_train(states, reward)]
+            action = actions[agent.act_and_train(np.transpose(states, [1, 2, 0]), clipped_reward)]
 
             state, reward, done, info = env.step(action)
 
+            if reward > 0:
+                clipped_reward = 1.0
+            elif reward < 0:
+                clipped_reward = -1.0
+            else:
+                clipped_reward = 0.0
             sum_of_rewards += reward
             step += 1
             global_step += 1
 
             if global_step % 10 ** 6 == 0:
-                path = os.path.join(args.outdir, '{}'.format(global_step))
-                agent.save(path)
+                path = os.path.join(args.outdir, '{}/model.ckpt'.format(global_step))
+                saver.save(sess, path)
 
         episode += 1
 

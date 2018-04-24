a322a1e0d5c30751edbd19b044a315660ffd202b
takuseno <takuma.seno@gmail.com>
2018-04-24 11:05:33 +0900

    Remove lightsaber function

--- build_graph.py
+++ build_graph.py
@@ -1,6 +1,12 @@
 import tensorflow as tf
-import lightsaber.tensorflow.util as util
 
+
+def huber_loss(loss, delta=1.0):
+    return tf.where(
+        tf.abs(loss) < delta,
+        tf.square(loss) * 0.5,
+        delta * (tf.abs(loss) - 0.5 * delta)
+    )
 
 def build_train(q_func,
                 num_actions,
@@ -21,13 +27,13 @@ def build_train(q_func,
 
         # q network
         q_t = q_func(obs_t_ph, num_actions, scope='q_func')
-        q_func_vars = util.scope_vars(
-            util.absolute_scope_name('q_func'))
+        q_func_vars = tf.get_collection(
+            tf.GraphKeys.TRAINABLE_VARIABLES, '{}/q_func'.format(scope))
 
         # target q network
         q_tp1 = q_func(obs_tp1_ph, num_actions, scope='target_q_func')
-        target_q_func_vars = util.scope_vars(
-            util.absolute_scope_name('target_q_func'))
+        target_q_func_vars = tf.get_collection(
+            tf.GraphKeys.TRAINABLE_VARIABLES, '{}/target_q_func'.format(scope))
 
         q_t_selected = tf.reduce_sum(q_t * tf.one_hot(act_t_ph, num_actions), 1)
         q_tp1_best = tf.reduce_max(q_tp1, 1)
@@ -35,7 +41,7 @@ def build_train(q_func,
 
         q_t_selected_target = rew_t_ph + gamma * q_tp1_best_masked
         td_error = q_t_selected - tf.stop_gradient(q_t_selected_target)
-        errors = tf.reduce_mean(util.huber_loss(td_error))
+        errors = tf.reduce_mean(huber_loss(td_error))
 
         # update parameters
         gradients = optimizer.compute_gradients(errors, var_list=q_func_vars)

331f5972ef5e7297d7128ca3384d05bf2c87e10c
takuseno <takuma.seno@gmail.com>
2018-10-27 00:58:29 +0900

    Add docker build scripts

---
+++ build-cpu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -eux
+
+sed -e 's/{image}/tensorflow\/tensorflow:latest-py3/g' Dockerfile_template > Dockerfile
+uid=$(id $(whoami) | awk '{print $1}' | sed -e 's/uid=//g' | sed -e 's/(.*$//g')
+gid=$(id $(whoami) | awk '{print $2}' | sed -e 's/gid=//g' | sed -e 's/(.*$//g')
+sudo docker build --build-arg uid=$uid --build-arg gid=$gid -t takuseno/dqn .
